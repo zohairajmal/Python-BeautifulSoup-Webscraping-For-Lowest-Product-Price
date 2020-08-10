c0f6a1a9520a0b78a79f6883134e2cb6fa358f6a
Zohair Ajmal <zohairajmal1@gmail.com>
2020-08-10 14:55:07 +0200

    Fixed functionality on linux by including a __init__.py. Cleaned up the main function

--- main.py
+++ main.py
@@ -5,29 +5,34 @@ from Websites.czone import Czone
 from Websites.pakDukaan import PakDukaan
 from makeExcel import MakeWorkbook
 
-# Purpose of this file:
-# 1) Call all the website classes and store their results in their corresponding class names
-# 2) Call the class to export the results to an excel workbook
+def main():
+	'''
+	# Purpose of this file:
+	# 1) Call all the website classes and store their results in their corresponding class names
+	# 2) Call the class to export the results to an excel workbook
+	'''
+	# Ensure search arguments are taken
+	if len(sys.argv[1]) <= 1:
+		print('Please input a search argument. Example: python main.py Power Supply')
+		exit()
 
-# Ensure search arguments are taken
-if len(sys.argv[1]) <= 1:
-	print('Please input a search argument. Example: python main.py Power Supply')
-	exit()
+	# Search argument list
+	search = sys.argv[1:]
 
-# Search argument list
-search = sys.argv[1:]
+	# Create a list of all the websites to search
+	websites = [IndusTech(search), EaseTec(search), Czone(search), PakDukaan(search)]
 
-# Create a list of all the websites to search
-websites = [IndusTech(search), EaseTec(search), Czone(search), PakDukaan(search)]
+	# Create an Excel Workbook
+	print('\nMaking Excel Workbook...')
+	MakeWorkbook(search, websites)
+	print('Done! \n\nOpen:', '_'.join(search) + '.xlsx\n')
 
-# Create an Excel Workbook
-print('\nMaking Excel Workbook...')
-MakeWorkbook(search, websites)
-print('Done! \n\nOpen:', '_'.join(search) + '.xlsx\n')
+	# Print number of results for each page
+	for site in websites:
+		if len(site.name) == 0:
+			print(site.website, 'return no results... Try something else?')
+		else:
+			print(site.website, 'found', len(site.name), 'results')
 
-# Print number of results for each page
-for site in websites:
-	if len(site.name) == 0:
-		print(site.website, 'return no results... Try something else?')
-	else:
-		print(site.website, 'found', len(site.name), 'results')
+if __name__ == "__main__": 
+	main()
